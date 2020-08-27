f3e28029be60346caf5d9851decc5dc386ebb8f6
Morten Jørgensen <mortenj2@hotmail.dk>
2020-08-27 19:16:13 +0200

    Man kan indsætte billeder nu

--- js/script.js
+++ js/script.js
@@ -3,19 +3,27 @@ $(function(){
   $("#nav-bar-placeholder").load("/nav-bar/nav-bar-index.html");
 });
 
-async function controlFunction() {
+async function controlFunction(){
     let data = await getData();
     insertData(data);
 }
 
-function getData() {
+function getData(){
     return fetch("/get/pictureCount/").then((response) => {
         return response.json();
     });
 }
 
-function insertData() {
-
+function insertData(data){
+    data.forEach(picture => {
+        console.log(picture);
+        const image = document.createElement('img');
+        image.src  = '/img/' + picture;
+        image.style.height = "35%";
+        image.style.width = "25%";
+        image.style.padding = "4%";
+        document.querySelector('.billeder').appendChild(image)
+    });
 }
 
 controlFunction();
